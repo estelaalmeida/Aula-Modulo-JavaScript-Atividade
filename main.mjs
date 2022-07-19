ec07fa77380b1323dac17509bf02905ac5453a63
Estela Almeida Oliveira <estela_almeida.oliveira@hotmail.com>
2022-07-19 18:39:11 -0300

    Aula atualizada

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby} from "./fucoes.mjs";
+
+console.log(mostraIdade("Estela", 32));
+console.log(mostraCidade("Estela","São josé dos Campos","SP"));
+console.log(mostraHobby("Estela","Ver filmes e Séries"));
